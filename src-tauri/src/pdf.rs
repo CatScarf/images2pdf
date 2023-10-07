4e7a126ccdd31baaf68c22d5ff887c3ba862a071
catscarf <catscarf98@gmail.com>
2023-10-07 14:20:57 +0800

    fix bug of image size and format

--- src-tauri/src/pdf.rs
+++ src-tauri/src/pdf.rs
@@ -28,7 +28,7 @@ impl Pdf {
 
     fn compress(&self, quality: u8) -> Vec<(Vec<u8>, u32, u32)>{
         // Get widths
-        let widths: Vec<u32> = self.imgs.par_iter()
+        let mut widths: Vec<u32> = self.imgs.par_iter()
             .map(|img| img.width()).collect();
 
         // Calculate median of widths
@@ -37,7 +37,6 @@ impl Pdf {
             1 => widths[0],
             2 => (widths[0] + widths[1]) / 2,
             _ => {
-                let mut widths = widths;
                 widths.sort();
                 widths[widths.len() / 2]
             }
@@ -47,7 +46,7 @@ impl Pdf {
         let img_bytes: Vec<(Vec<u8>, u32, u32)> = self.imgs.par_iter().map(|img| {
             let nwidth = median;
             let nheight = (img.height() as f32 * median as f32 / img.width() as f32) as u32;
-            img.resize_exact(nwidth, nheight, image::imageops::FilterType::Nearest);
+            let img = img.resize_exact(nwidth, nheight, image::imageops::FilterType::Nearest).to_rgb8();
             let mut img_bytes = Cursor::new(vec![]);
             img.write_to(&mut img_bytes, image::ImageOutputFormat::Jpeg(quality)).unwrap();
             (img_bytes.into_inner(), nwidth, nheight)
@@ -70,8 +69,6 @@ impl Pdf {
         for (data, width, height) in img_data.iter() {
             let x_cm = (*width as f64 * 2.54 / 300.0 * 100.0) as u32;
             let y_cm = (*height as f64 * 2.54 / 300.0 * 100.0) as u32;
-            let x_pt: i32 = (x_cm as f64 * 0.3528) as i32;
-            let y_pt: i32 = (y_cm as f64 * 0.3528) as i32;
             let resources = dictionary! {
                 "XObject" => dictionary! {
                     "Image1" => doc.add_object(Stream::new(dictionary! {
@@ -91,7 +88,7 @@ impl Pdf {
             let content = Content {
                 operations: vec![
                     Operation::new("q", vec![]), 
-                    Operation::new("cm", vec![Object::Integer(x_pt as i64), Object::Integer(0), Object::Integer(0), Object::Integer(y_pt as i64), Object::Integer(0), Object::Integer(0)]),
+                    Operation::new("cm", vec![Object::Integer(x_cm as i64), Object::Integer(0), Object::Integer(0), Object::Integer(y_cm as i64), Object::Integer(0), Object::Integer(0)]),
                     Operation::new("Do", vec![Object::Name("Image1".as_bytes().to_vec())]), 
                     Operation::new("Q", vec![]),
                 ],
@@ -103,7 +100,7 @@ impl Pdf {
                 "Parent" => pages_id,
                 "Contents" => content_id,
                 "Resources" => resources,
-                "MediaBox" => vec![0.into(), 0.into(), x_pt.into(), y_pt.into()],
+                "MediaBox" => vec![0.into(), 0.into(), x_cm.into(), y_cm.into()],
             });
             num_pages += 1;
             kids.push(page_id);
